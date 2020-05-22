3abc47adea6a3504ce5a6c2281a99ccdb81bdeff
zwh <841082829@qq.com>
2020-05-22 14:43:12 +0800

    test

--- config/config.js
+++ config/config.js
@@ -6,6 +6,15 @@ export default defineConfig({
   },
   routes: [
     { path: '/', component: '@/pages/layout', title: 'test' },
-    { path: '/page', component: '@/pages/projectAdd', title: '新建项目' },
+    {
+      path: '/page',
+      component: '@/pages/project/project-manage/projectAdd',
+      title: '新建项目',
+    },
+    {
+      path: '/detail',
+      component: '@/pages/project/project-manage/projectDetail',
+      title: '项目详情',
+    },
   ],
 });
---
+++ src/pages/project/project-manage/projectDetail/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class projectDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return 123;
+  }
+}
+
+export default projectDetail;
